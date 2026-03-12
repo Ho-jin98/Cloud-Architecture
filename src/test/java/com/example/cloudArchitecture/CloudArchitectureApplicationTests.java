eb9dad08005d150d8b14da551e8d84fcc49ceadd
Ho-jin98 <hohoj98@naver.com>
2026-03-12 20:40:13 +0900

    feat: check commit

---
+++ src/test/java/com/example/cloudArchitecture/CloudArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudArchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

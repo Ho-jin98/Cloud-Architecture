57a1949f3e89109e7c1a35abcb466cba6487991b
Ho-jin98 <hohoj98@naver.com>
2026-03-12 20:36:35 +0900

    feat: S3Config, S3Uploader 추가

--- src/main/java/com/example/cloudArchitecture/common/S3Uploader.java
+++ src/main/java/com/example/cloudArchitecture/common/S3Uploader.java
@@ -1,4 +1,110 @@
 package com.example.cloudArchitecture.common;
 
+import com.example.cloudArchitecture.exception.ErrorCode;
+import com.example.cloudArchitecture.exception.ServerException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
 public class S3Uploader {
+
+    private final S3Client s3Client;
+
+    private final S3Presigner s3Presigner;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    // 업로드 메서드
+    public String upload(MultipartFile image) {
+        if (image == null || image.isEmpty()) return null;
+        // 이미지가 없거나 비어있으면 null 반환
+        try {
+            // S3 에 저장할 이름 (UUID + 확장자)
+            String originalImageName = image.getOriginalFilename();
+            // ex) 홍길돌.jpg
+            String extension = "";
+            if (originalImageName != null && originalImageName.contains("."))
+            {
+                extension =
+                        originalImageName.substring(originalImageName.lastIndexOf("."));
+            }
+            // → ".jpg"
+            // lastIndexOf(".") → 마지막 "." 위치를 찾아서
+            // substring()으로 "." 포함 뒤에 있는 확장자만 추출
+
+            String imageNameForS3 = "images/" + UUID.randomUUID() +
+                    extension;
+            // → "images/랜덤 UUID값.jpg"
+            // images/ 폴더 안에 UUID + 확장자로 저장
+
+            // S3 에 업로드
+            // PutObjectRequest -> "이런 조건으로 S3에 저장해줘" 라는 요청 명세서
+            PutObjectRequest request = PutObjectRequest.builder()
+                    .bucket(bucket) // 어느 버킷에 저장할지
+                    .key(imageNameForS3) // S3에 저장될 파일 경로/이름
+                    .contentType(image.getContentType()) // "image/jpeg" 등 파일 타입
+                    .contentLength(image.getSize()) // 파일 크기 (byte)
+                    .build();
+
+            // 실제 S3에 업로드
+            s3Client.putObject(request, RequestBody.fromInputStream(
+                    image.getInputStream(), image.getSize()));
+            //request → 아까 만든 요청 명세서
+            //RequestBody.fromInputStream() → 실제 이미지 데이터를 스트림으로 변환
+            //image.getInputStream() → 이미지 파일의 실제 데이터
+            //image.getSize() → 파일 크기
+
+            // URL 반환
+            return imageNameForS3;
+            // S3에 저장된 이미지의 주소를 조합해서 반환
+        } catch (IOException e) {
+            throw new ServerException(ErrorCode.IMAGE_FAIL);
+            // 전체 흐름
+            // 이미지 받음 → UUID로 파일명 생성 → S3 업로드 요청 → 실제 업로드 → URL 반환
+        }
+    }
+
+    public String getPresignedUrl(String imageName) {
+        // 어떤 객체에 대한 URL을 만들지 요청 객체 생성
+        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                .bucket(bucket) // 어느 버킷에서 가져올지
+                .key(imageName) // S3에 지정된 파일 경로
+                .build();
+
+        // Presign 요청 만들기
+        GetObjectPresignRequest getObjectPresignRequest = GetObjectPresignRequest.builder()
+                .signatureDuration(Duration.ofDays(7)) // 7일 후 만료
+                .getObjectRequest(getObjectRequest) // 위에서 만든 요청 객체 연결
+                .build();
+        // Duration.ofDays() -> 자바가 제공하는 시간표현 클래스
+        // Duration.ofDays(7) -> 7일
+        // Duration.ofHours(24) -> 24시간
+        // Duration.ofMinutes(30) -> 30분
+
+        // Presign URL 생성
+        PresignedGetObjectRequest presignedGetObjectRequest = s3Presigner.presignGetObject(getObjectPresignRequest);
+
+        // URL 반환
+        return presignedGetObjectRequest.url().toString();
+    }
 }
+
+
--- src/main/java/com/example/cloudArchitecture/config/S3Config.java
+++ src/main/java/com/example/cloudArchitecture/config/S3Config.java
@@ -1,4 +1,50 @@
 package com.example.cloudArchitecture.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+
+@Configuration
 public class S3Config {
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.AP_NORTHEAST_2)
+                .build();
+    }
+
+    @Bean
+    public S3Presigner s3Presigner() {
+        return S3Presigner.builder()
+                .region(Region.AP_NORTHEAST_2)
+                .build();
+    }
 }
+
+/* S3Client는 AWS SDK에서 제공하는 클래스인데,
+ build.gradle에 추가한 implementation 'io.awspring.cloud:spring-cloud-aws-starter-s3'
+ 해당 의존성이 추가될 때 같이 들어옴, 참고로 내부적으로 AWS SDK (software.amazon.awssdk:s3)를 포함하고 있음,*/
+
+/* 빌더 (builder)는 갑자기 어떻게 나온 걸까?
+ -> AWS SDK 내부에서 이미 빌더 패턴으로 설계되어 있어서 저렇게 호출할 수 있음
+ AWS 개발자들이 S3Client를 만들 때 빌더 패턴으로 설계를해놓을 것!
+* S3Client -> AWS SDK에서 제공하는 클래스
+* builder() -> AWS SDK 내부에서 이미 빌더 패턴으로 설계되어 있어서 사용 가능! */
+
+/* 왜 region만 추가를 할까?
+ S3Client가 AWS에 요청을 보낼 때 "어느 지역 서버로 보낼지"만 알면 됨,
+ (만약, region을 안넣으면 어느 서버로 요청해야 할지 몰라서 에러 발생)
+
+ 나머지 인증 정보 (EC2, RDS 등등)는 왜 안넣을까?
+ IAM Role 방식이라서 AWS가 자동으로 처리해줌!
+ EC2 시작 -> AWS가 EC2에 임시 자격증명 자동 발급 -> S3Client가 그 자격증명을 자동으로 가져다 사용
+ -> 개발자는 아무것도 안 해도 됨!
+ 이것을 AWS DefaultCredentialsProvider 라고 부른다
+ S3Client는 내부적으로 자격증명을 다음과 같은 순서로 자동 탐지
+ 1. 환경 변수
+ 2. yml 설정 값
+ 3. EC2 IAM Role 그래서 코드에 따로 안 넣어도 EC2 IAM Role이 있으면 자동으로 인증됨!!!! */
+
+
